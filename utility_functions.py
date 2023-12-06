d2a21e72e56cf70d3910631df64485bb48ba4aeb
buttegggggggg <150787738+buttegggggggg@users.noreply.github.com>
2023-12-07 00:54:28 +0800

    Add files via upload

--- utility_functions.py
+++ utility_functions.py
@@ -22,9 +22,22 @@ class IBMQ_backends:
     @classmethod
     def least_busy(cls):
         return ibmq.least_busy(cls.provider.backends(simulator = False))
+    
+    @classmethod
+    def simulator(cls):
+        return ibmq.least_busy(cls.provider.backends(simulator = True))
 
 def run_circuits(start, stop, get_backend, show_plot, each_shot=False):
-    # 創建一個量子電路
+    """創建一個量子電路
+
+    1 1 1 1 1 1 1 ...  -> [[[[[[[[1] 2] 3] 4] 5] 6] 7] 
+      2 2 2 2 2 2
+        3 3 3 3 3
+          4 4 4 4
+            5 5 5
+              6 6
+                7
+    """
     qc = QuantumCircuit(1, 1)
     qc.h(0)  # 添加 H-gate
     qc.measure([0], [0])
@@ -39,6 +52,7 @@ def run_circuits(start, stop, get_backend, show_plot, each_shot=False):
             print(f"Using {backend.name()}")
             # 獲取結果為 0 的次數並除以 n
             result = backend.run(transpile(qc,  backend=backend, optimization_level=3), shots=n, memory=each_shot).result()
+            # 增加 memory=each_shot （每次結果都記錄，並非僅記錄次數）
 
             def 打印結果列(results_list):
                 print('n =', len(results_list))
@@ -54,3 +68,30 @@ def run_circuits(start, stop, get_backend, show_plot, each_shot=False):
                 results_list.append(result.get_counts(qc).get('0', 0) / n)
                 打印結果列(results_list)
         run_circuit()
+
+def run_circuits(start, stop, get_backend):
+    qc = QuantumCircuit(1, 1)
+    qc.h(0)  # 添加 H-gate
+    qc.measure([0], [0])
+
+    n = int(stop)
+    
+    result_list = []
+    
+    backend = get_backend()
+    print(f"Using {backend.name()}")
+   
+    result = backend.run(transpile(qc,  backend=backend ,optimization_level=3),  shots=n, memory=True).result()
+    
+    # 獲得每次測量的結果
+    measurement_results = result.get_memory()
+    print('measurement results =' ,measurement_results)
+    for i in range(start-1, stop):
+        ith_list = measurement_results[start-1 : i+1]
+        ith_0_num = ith_list.count('0')
+        ith_0_prob = ith_0_num/(i+1)
+
+        #print(ith_list,ith_0_num,ith_0_prob )
+        result_list.append(ith_0_prob)
+    print('probability of 0 =',result_list)
+    plot_results(result_list)
